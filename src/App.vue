a7a7b9010ee610584f02fcd95fd7c5e708f77277
wayne <wayne1024.cn@outlook.com>
2022-10-28 16:50:51 +0800

    修改布局

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,27 @@
 <template>
   <div id="app">
-    <div class="header"></div>
+    <!-- 标头 -->
+    <div class="header">
+      <dv-border-box-10>生产进度现况板</dv-border-box-10>
+    </div>
+
+    <!-- 主体 -->
     <div class="content">
       <div class="left">
-        <div class="box1">
-          <!-- <dv-border-box-1></dv-border-box-1> -->
-        </div>
-        <div class="box2">
-          <!-- <dv-border-box-9></dv-border-box-9> -->
-        </div>
+        <dv-border-box-11 class="real_time_month" title="月别实时进度">
+          <div class="box1"></div>
+          <div class="box2"></div>
+        </dv-border-box-11>
+        <dv-border-box-11 class="currt_status" title="在工现况">
+          <CurrentStatus></CurrentStatus>
+        </dv-border-box-11>
       </div>
       <div class="right">
-        <div class="box3">
-          <!-- <dv-border-box-9></dv-border-box-9> -->
-        </div>
-        <div class="box4">
-          <!-- <dv-border-box-1></dv-border-box-1> -->
-        </div>
+        <dv-border-box-11 class="real_time_day" title="日别实时进度">
+        </dv-border-box-11>
+        <dv-border-box-11 class="real_time_hour" title="小时别实时进度">
+          
+        </dv-border-box-11>
       </div>
     </div>
   </div>
@@ -25,7 +30,10 @@
 <script>
 
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    CurrentStatus: () => import("./components/CurrentStatus.vue")
+  }
 }
 </script>
 
@@ -39,13 +47,18 @@ export default {
   display: flex;
   align-items: center;
   flex-direction: column;
-
+  background-image: url('../src/assets/screen.png');
+  width: 100vw;
+  height: 100vh;
 }
 
 .header {
   width: 90vw;
   height: 8vh;
-  background-color: gainsboro;
+  text-align: center;
+  color: white;
+  /* background-color: gainsboro; */
+  /* opacity: 0.1; */
 }
 
 .content {
@@ -64,31 +77,48 @@ export default {
   flex-direction: column;
 }
 
-.box1 {
+.real_time_month {
   width: 44vw;
   height: 89vh;
-  background-color: gainsboro;
+  /* background-color: rgba(220, 220, 220, 0.103); */
   margin-bottom: 1vh;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+}
+
+.box1 {
+  width: 43vw;
+  height: 27vh;
+  margin-top: 1vh;
+  /* background-color: rgba(229, 233, 223, 0.466); */
 }
 
 .box2 {
+  width: 43vw;
+  height: 23vh;
+  margin-top: 1vh;
+  /* background-color: rgba(229, 233, 223, 0.466); */
+}
+
+.currt_status {
   width: 44vw;
   height: 59vh;
-  background-color: gainsboro;
+  /* background-color: rgba(220, 220, 220, 0.103); */
   margin-bottom: 1vh;
 }
 
-.box3 {
+.real_time_day {
   width: 44vw;
   height: 59vh;
-  background-color: gainsboro;
+  /* background-color: rgba(220, 220, 220, 0.144); */
   margin-bottom: 1vh;
 }
 
-.box4 {
+.real_time_hour {
   width: 44vw;
   height: 89vh;
-  background-color: gainsboro;
+  /* background-color: rgba(220, 220, 220, 0.11); */
   margin-bottom: 1vh;
 }
 
---
+++ src/components/CurrentStatus.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="current_status">
+    <div class="current_status_container"></div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+#current_status{
+  color: white;
+  /* background-color: rgba(127, 255, 212, 0.164); */
+  width: 100%;
+  height: 100%;
+  /* margin-top: 10%; */
+  display: flex;
+  /* align-items: center; */
+  justify-content: center;
+}
+
+.current_status_container{
+  width: 80%;
+  height: 70%;
+  position: relative;
+  top: 8vh;
+  background-color: rgba(127, 255, 212, 0.164);
+}
+</style>

3fc11d9984b71f181f74ec5dc1fe73fb933040a0
wayne <wayne1024.cn@outlook.com>
2022-10-31 00:22:30 +0800

    添加Antd-Vue依赖，并全局引入。

--- src/main.js
+++ src/main.js
@@ -3,7 +3,10 @@ import App from './App.vue'
 // 将自动注册所有组件为全局组件
 import dataV from '@jiaminghi/data-view'
 import * as echarts from 'echarts'
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css';
 
+Vue.use(Antd)
 Vue.prototype.$echarts = echarts;
 Vue.use(dataV)
 Vue.config.productionTip = false
